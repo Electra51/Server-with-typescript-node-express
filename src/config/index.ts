8023e61c36b66dec0a4f6e6f3f7ecf5a1ac35d7f
Safayet Nur <safayetnurelectra@gmail.com>
2026-09-25 07:17:53 +0600

    fix:db connection, dotenv path

--- src/config/index.ts
+++ src/config/index.ts
@@ -1,9 +1,10 @@
 import dotenv from "dotenv";
+import path from "path";
 
-dotenv.config();
+dotenv.config({ path: path.join(process.cwd(), ".env") });
 
 const config = {
-  port: process.env.PORT ? Number(process.env.PORT) : 5000,
+  port: Number(process.env.PORT) || 5000,
   databaseUrl: process.env.DATABASE_URL || "",
 };
 
